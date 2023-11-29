5ac5fa91c7054664a7d82f734dccfa4f24d721c8
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-11-29 16:46:03 +0100

    feat: added google analyser, implementing IAnalyser

---
+++ VisionTest/GoogleAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Vision.V1;
+using VisionTest.Interfaces;
+
+namespace VisionTest
+{
+    public class GoogleAnalyser : IAnalyser
+    {
+        ImageAnnotatorClient client;
+        public GoogleAnalyser() 
+        {
+             client = ImageAnnotatorClient.Create();
+        }
+        public IImageData Analyse(string imageString, int labelAmount = 3, int maxConfidenceAmount = 10)
+        {
+            List<string> labels = new List<string>();
+            List<float> confidences = new List<float>();
+            Image image = Image.FromFile(imageString);
+            IReadOnlyList<EntityAnnotation> results = client.DetectLabels(image, maxResults:labelAmount);
+
+            int index = 0;
+            foreach (EntityAnnotation result in results)
+            {
+                
+                if(result.Score >= maxConfidenceAmount || index <= labelAmount)
+                {
+                    confidences.Add(result.Score);
+                    labels.Add(result.Description);
+                }
+                index++;
+            }
+            return new GoogleImageData(imageString, image, labels.ToArray(), confidences.ToArray());
+        }
+    }
+}
