d61c4553fef9e6f46a133056435d8cd7d328bd42
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-11-29 16:45:36 +0100

    feat: changed analyser to interface

--- VisionTest/Analyser.cs
+++ VisionTest/Analyser.cs
@@ -3,36 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Google.Cloud.Vision.V1;
+using VisionTest.Interfaces;
 
 namespace VisionTest
 {
-    public class Analyser
+    public interface IAnalyser
     {
-        ImageAnnotatorClient client;
-        public Analyser() 
-        {
-             client = ImageAnnotatorClient.Create();
-        }
-        public ImageData Analyse(string imageString, int labelAmount = 3, int maxConfidenceAmount = 10)
-        {
-            List<string> labels = new List<string>();
-            List<float> confidences = new List<float>();
-            Image image = Image.FromFile(imageString);
-            IReadOnlyList<EntityAnnotation> results = client.DetectLabels(image, maxResults:labelAmount);
-
-            int index = 0;
-            foreach (EntityAnnotation result in results)
-            {
-                
-                if(result.Score >= maxConfidenceAmount || index <= labelAmount)
-                {
-                    confidences.Add(result.Score);
-                    labels.Add(result.Description);
-                }
-                index++;
-            }
-            return new ImageData(image, labels.ToArray(), confidences.ToArray());
-        }
+        public abstract IImageData Analyse(string imageString, int labelAmount = 3, int maxConfidenceAmount = 10);
     }
 }
