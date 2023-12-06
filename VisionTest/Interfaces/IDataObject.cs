7e6c50989867632ca812d5f647c8268c0d3502c1
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-12-06 14:59:19 +0100

    Create IDataObject.cs

---
+++ VisionTest/Interfaces/IDataObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionTest.Interfaces
+{
+    public interface IDataObject
+    {
+        public bool DoesObjectExist();
+
+        public void UploadObject(IImageData image, string remoteFullPath);
+
+        public IImageData DownloadObject(IImageData image, string localFullPath);
+
+        public string PublishObject(string remoteFullPath, int expirationTime = 90);
+
+        public void RemoveObject(string remoteFullPath, bool recursive = false);
+
+    }
+}
