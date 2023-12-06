0676c5a97107085f757ac839b16e0e1bd70b0034
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-12-06 14:58:45 +0100

    feat: renamed all tests

--- VisionTestTests/TestGoogleAnalyser.cs
+++ VisionTestTests/TestGoogleAnalyser.cs
@@ -9,30 +9,30 @@ namespace VisionTestTests
         string imageSource = "20230727_140005.jpg";
         public IImageData img;
         GoogleAnalyser analyser;
+        const int MAX_LABELCOUNT = 3;
 
         [TestInitialize()]
         public void Startup()
         {
             analyser = new GoogleAnalyser();
-            img = analyser.Analyse(imageSource,3, 50);
         }
 
         [TestMethod]
-        public void DefaultTest()
+        public void Analyse_DefaultImage_IsntNull()
         {
             IImageData img = analyser.Analyse(imageSource);
             Assert.IsNotNull(img);
         }
 
         [TestMethod]
-        public void LabelTest()
+        public void Analyse_ImageWithMaxLabelCount_ReturnsThree()
         {
-            IImageData img = analyser.Analyse(imageSource, 3);
-            Assert.IsTrue(img.Labels.Count() >= this.img.Labels.Count());
+            IImageData img = analyser.Analyse(imageSource, MAX_LABELCOUNT);
+            Assert.IsTrue(img.Labels.Count() >= 3);
         }
 
         [TestMethod]
-        public void ConfidenceTest()
+        public void Analyse_ImageWithMaxConfidence_ReturnsBelowMaxConfidence()
         {
             int maxConf = 50;
             bool hasPassed = true;
@@ -49,11 +49,11 @@ namespace VisionTestTests
         }
 
         [TestMethod]
-        public void ConfidenceAndLabelTest()
+        public void Analyse_ImageWithMaxConfidenceAndMaxLabel_ReturnsBelowMaxConfidenceAndLabelCount()
         {
             int maxConf = 50;
             bool hasPassed = true;
-            IImageData img = analyser.Analyse(imageSource,3,maxConf);
+            IImageData img = analyser.Analyse(imageSource, MAX_LABELCOUNT, maxConf);
 
             foreach (var conf in img.Confidences)
             {
@@ -62,7 +62,7 @@ namespace VisionTestTests
                     hasPassed = false;
                 }
             }
-            Assert.IsTrue(hasPassed && img.Labels.Count() >= this.img.Labels.Count());
+            Assert.IsTrue(hasPassed && img.Labels.Count() >= MAX_LABELCOUNT);
         }
     }
 }
