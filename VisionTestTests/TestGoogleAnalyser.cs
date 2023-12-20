648c86b0a27ac262591efb2ec0ed6b32dc41a69b
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-12-20 16:12:21 +0100

    fix: overwrote old test with new test specs

--- VisionTestTests/TestGoogleAnalyser.cs
+++ VisionTestTests/TestGoogleAnalyser.cs
@@ -1,84 +1,116 @@
+using Google.Api;
 using VisionTest.Analysers;
 using VisionTest.Interfaces;
+using static Google.Rpc.Context.AttributeContext.Types;
 
 namespace VisionTestTests
 {
     [TestClass]
     public class TestGoogleAnalyser
     {
-        string imageSource = "20230727_140005.jpg";
+        string localFile = "20230727_140005.jpg";
+        string remoteFileUrl = "20230727_140005.jpg";
+        int maxLabels = 3;
         public IImageData img;
-        GoogleLabelDetectorImpl analyser;
+        GoogleLabelDetectorImpl labelDetector;
         const int MAX_LABELCOUNT = 3;
 
         [TestInitialize()]
         public void Startup()
         {
-            analyser = new GoogleLabelDetectorImpl();
+            labelDetector = new GoogleLabelDetectorImpl();
         }
 
         [TestMethod]
-        public void Analyse_DefaultImage_IsntNull()
+        public async Task Analyze_LocalFileWithDefaultValues_ImageAnalyzed()
         {
             //given
-            IImageData img = analyser.Analyse(imageSource);
-
-            //then
-            Assert.IsNotNull(img);
-        }
-
-        [TestMethod]
-        public void Analyse_ImageWithMaxLabelCount_ReturnsThree()
-        {
-            //given
-            IImageData img = analyser.Analyse(imageSource, MAX_LABELCOUNT);
-
-            //then
-            Assert.IsTrue(img.Labels.Count() >= 3);
-        }
-
-        [TestMethod]
-        public void Analyse_ImageWithMaxConfidence_ReturnsBelowMaxConfidence()
-        {
-
-            //given
-            int maxConf = 50;
-            bool hasPassed = true;
-            IImageData img = analyser.Analyse(imageSource, maxConfidenceAmount: maxConf);
+            Assert.IsTrue(File.Exists(localFile));
 
             //when
-            foreach (var conf in img.Confidences) 
-            { 
-                if(conf > maxConf)
-                {
-                    hasPassed = false;
-                }
-            }
+            IImageData response =  this.labelDetector.Analyze(localFile);
+            //TODO the type of response contains the payload (returned in json by the api)
 
             //then
-            Assert.IsTrue(hasPassed);
-        }
-
-        [TestMethod]
-        public void Analyse_ImageWithMaxConfidenceAndMaxLabel_ReturnsBelowMaxConfidenceAndLabelCount()
-        {
-            //given
-            int maxConf = 50;
-            bool hasPassed = true;
-            IImageData img = analyser.Analyse(imageSource, MAX_LABELCOUNT, maxConf);
-
-
-            //when
-            foreach (var conf in img.Confidences)
+            Assert.IsTrue(response.Labels.Count() <= 10);
+            foreach (int metric in response.Confidences)
             {
-                if (conf > maxConf)
-                {
-                    hasPassed = false;
-                }
+                Assert.IsTrue(metric >= 90);
             }
+        }
+        [TestMethod]
+        public async Task Analyze_RemoteImageWithDefaultValues_ImageAnalyzed()
+        {
+            //given
+            //TODO test if the remote file is available
+
+            //when
+            IImageData response = this.labelDetector.Analyze(remoteFileUrl);
+            //TODO the type of response contains the payload (returned in json by the api)
 
             //then
-            Assert.IsTrue(hasPassed && img.Labels.Count() >= MAX_LABELCOUNT);
+            Assert.IsTrue(response.Labels.Count() <= 10);
+            foreach (int metric in response.Confidences)
+            {
+                Assert.IsTrue(metric >= 90);
+            }
+        }
+        [TestMethod]
+        public async Task Analyze_RemoteImageWithCustomMaxLabelValue_ImageAnalyzed()
+        {
+            //given
+            //TODO test if the remote file is available
+            int maxLabel = 5;
+
+            //when
+            IImageData response =  this.labelDetector.Analyze(remoteFileUrl, maxLabels);
+            //TODO the type of response contains the payload (returned in json by the api)
+
+            //then
+            Assert.IsTrue(response.Labels.Count() <= maxLabels);
+            foreach (int metric in response.Confidences)
+            {
+                Assert.IsTrue(metric >= 90);
+            }
+        }
+
+        [TestMethod]
+        public async Task Analyze_RemoteImageWithCustomMinConfidenceLevelValue_ImageAnalyzed()
+        {
+            //given
+            //TODO test if the remote file is available
+            int minConfidenceLevel = 60;
+
+            //when
+            IImageData response =  labelDetector.Analyze(remoteFileUrl, minConfidenceLevel);
+            //TODO the type of response contains the payload (returned in json by the api)
+
+            //then
+            Assert.IsTrue(response.Labels.Count() <= 10);
+            foreach (int metric in response.Confidences)
+            {
+                Assert.IsTrue(metric >= minConfidenceLevel);
+            }
+        }
+
+        [TestMethod]
+        public async Task Analyze_RemoteImageWithCustomValues_ImageAnalyzed()
+        {
+            //given
+            //TODO test if the remote file is available
+            int maxLabels = 5;
+            int minConfidenceLevel = 60;
+
+            //when
+            IImageData response =  this.labelDetector.Analyze(remoteFileUrl, maxLabels, minConfidenceLevel);
+            //TODO the type of response contains the payload (returned in json by the api)
+
+            //then
+            Assert.IsTrue(response.Labels.Count() <= maxLabels);
+            foreach (int metric in response.Confidences)
+            {
+                Assert.IsTrue(metric >= minConfidenceLevel);
+            }
         }
     }
 }
